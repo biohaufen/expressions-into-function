38e919a6126ac67e3f2447a91dbadf0320a99544
David Herbst Ayala <mail@david-it.dev>
2021-03-09 16:30:21 +0100

    implemented personalIntroduction

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,28 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
 
+const personalIntroduction = (name, height, favoriteColor) => `My name is ${name}, and my height is ${height}, and my favorite color is ${favoriteColor}`;
+
 var name = "Bob";
 var height = "72";
 var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(personalIntroduction(name, height, favoriteColor));
+
 
 var name = "Samantha";
 var height = "65";
 var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(personalIntroduction(name, height, favoriteColor));
+
 
 var name = "Christopher";
 var height = "69";
 var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(personalIntroduction(name, height, favoriteColor));
+
 
 var name = "Tamera";
 var height = "63";
 var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+console.log(personalIntroduction(name, height, favoriteColor));
+
